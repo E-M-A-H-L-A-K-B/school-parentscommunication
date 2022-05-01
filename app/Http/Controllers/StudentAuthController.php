3ec3b6ff3c145937e4b62b30907a1fd17e9a01d8
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2022-05-02 00:01:20 +0300

    Added Functionality To StudentAuth And UserAuth

--- app/Http/Controllers/StudentAuthController.php
+++ app/Http/Controllers/StudentAuthController.php
@@ -3,8 +3,83 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Student;
+use Illuminate\Support\Str;
 class StudentAuthController extends Controller
 {
-    //
+    public function login(Request $req)
+    {
+        return view('studentlogin');
+    }
+
+    public function ChangePassword(Request $req)
+    {
+        return view('student change password');
+    }
+
+    public function HandleChangePassword(Request $req)
+    {
+        $user = Student::where('id',$req->id)->first();
+        if(Hash::check($req->oldpass,$user->password))
+        {
+            $user->password = Hash::make($req->newpass);
+            $user->save();
+            return redirect()->intended('/')->with('change_success','Password Changed Successfully');
+        }
+
+        else
+        {
+            return back()->with('error','The Old Password Is Not Right');
+        }
+    }
+
+    public function register(Request $req)
+    {
+        $credentials= $req->validate([
+            'name' => ['required'],
+            'last_name' => ['required'],
+            'father' => ['required'],
+        ]);
+
+        $password = Str::random(8);
+
+        $new = new Student();
+
+        $new->name = $req->name;
+        $new->father = $req->father;
+        $new->last_name = $req->last_name;
+        $new->password = Hash::make($password);
+
+        $new->save();
+
+        return back()->with('student_success','Student Registered Successfully,Password is: '.$password);
+    }
+
+    public function Handlelogin(Request $req)
+    {
+        $credentials= $req->validate([
+            'name' => ['required'],
+            'last_name' => ['required'],
+            'father' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if(Auth::guard('student')->attempt(['name'=> $req->name , 'last_name'=> $req->last_name, 'father'=> $req->father, 'password'=> $req->password]))
+        {
+            $req->session()->regenerate();
+ 
+            return redirect()->intended('/');
+        }
+
+        return back();
+    }
+
+    public function logout()
+    {
+        Auth::guard('student')->logout();
+
+        return redirect()->intended('/');
+    }
 }
--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -3,8 +3,84 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class UserAuthController extends Controller
 {
-    //
+    public function login(Request $req)
+    {
+        return view('userlogin');
+    }
+
+    public function register(Request $req)
+    {
+        $credentials= $req->validate([
+            'name' => ['required'],
+            'last_name' => ['required'],
+            'father' => ['required'],
+        ]);
+
+        $password = Str::random(8);
+
+        $new = new User();
+
+        $new->name = $req->name;
+        $new->father = $req->father;
+        $new->last_name = $req->last_name;
+        $new->password = Hash::make($password);
+
+        $new->save();
+
+        return back()->with('user_success','User Registered Successfully,Password is: '.$password);
+    }
+
+    public function ChangePassword(Request $req)
+    {
+        return view('user change password');
+    }
+
+    public function HandleChangePassword(Request $req)
+    {
+        $user = User::where('id',$req->id)->first();
+        if(Hash::check($req->oldpass,$user->password))
+        {
+            $user->password = Hash::make($req->newpass);
+            $user->save();
+            return redirect()->intended('/')->with('change_success','Password Changed Successfully');
+        }
+
+        else
+        {
+            return back()->with('error','The Old Password Is Not Right');
+        }
+    }
+
+    public function Handlelogin(Request $req)
+    {
+        $credentials= $req->validate([
+            'name' => ['required'],
+            'last_name' => ['required'],
+            'father' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if(Auth::attempt(['name'=> $req->name , 'last_name'=> $req->last_name, 'father'=> $req->father, 'password'=> $req->password]))
+        {
+            $req->session()->regenerate();
+ 
+            return redirect()->intended('/');
+        }
+
+        return back()->with('error','The Login Credentials Do Not Match With Our Database');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->intended('/');
+    }
 }
